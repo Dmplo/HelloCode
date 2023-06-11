8e333d2021e2fced084fb9a09e1ab4ac84086a33
side <dmplo@mail.ru>
2023-06-11 17:57:38 +0700

    add Example 010 MethodArray

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = {1, 12, 31, 4, 15, 61, 71, 84};
+
+int count = arr.Length;
+
+int find = 84;
+
+int index = 0;
+
+while (index < count) {
+    if(arr[index] == find) {
+        Console.WriteLine(index);
+    }
+    index++;
+}
+
